082505fd5950db5eeec34ad2617306dfdf3936f6
Tomas Zeman <tomas.zeman@gmail.com>
2018-09-09 16:09:10 +0200

    update

--- src/main/java/me/zeman/springboot1/service/WeatherService.java
+++ src/main/java/me/zeman/springboot1/service/WeatherService.java
@@ -4,10 +4,12 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import me.zeman.springboot1.SpringBootDemo1Application;
 import org.springframework.stereotype.Component;
 import us.monoid.web.Resty;
 
 import java.io.IOException;
+import java.util.Random;
 
 @Component
 public class WeatherService {
@@ -19,25 +21,27 @@ public class WeatherService {
         String hello = "";
 
         Random rnd = new Random();
-        int number = rnd.nextInt(20);
+        int number = rnd.nextInt(10);
 
         Resty rest = new Resty();
+
         try {
 
             if (number==1){
-
-                throw new Exception("Tomas Crazy Exception");
-
+                throw new Exception("Weather Crazy Exception");
             }
-             hello = rest.text("http://s2.cotopia.com:8888/weather/" +c1 +"/" + c2).toString();
+
+             hello = rest.text(SpringBootDemo1Application.W_URL + c1 +"/" + c2).toString();
 
         } catch (Exception e) {
-             hello = r.text(SpringBootDemo1Application.W_URL + c1 +"/" + c2).toString();
-        } catch (IOException e) {
+            System.out.println("Weather exception: " + e.toString());
             e.printStackTrace();
+
         }
 
         return hello;
     }
 
 }
+
+
